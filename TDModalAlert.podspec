4b779b4a1270de150a75a39705e25f4db8b057f1
Duong Dinh <duongdinh@duongs-mbp.clance.local>
2019-12-03 15:03:35 +0100

    Version 1

--- TDModalAlert.podspec
+++ TDModalAlert.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'TDModalAlert'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of TDModalAlert.'
+  s.summary          = 'A customized modal alert for iOS'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,19 +18,21 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+A customized modal alert for iOS written in Swift 5
                        DESC
 
-  s.homepage         = 'https://github.com/duongdh93@gmail.com/TDModalAlert'
+  s.homepage         = 'https://github.com/duongdinhhong/TDModalAlert'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'duongdh93@gmail.com' => 'duongdh93@gmail.com' }
-  s.source           = { :git => 'https://github.com/duongdh93@gmail.com/TDModalAlert.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/duongdinhhong/TDModalAlert.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'TDModalAlert/Classes/**/*'
+  s.source_files = 'TDModalAlert/Classes/TDModalAlertView.swift'
+  
+  s.swift_versions = '5.0'
   
   # s.resource_bundles = {
   #   'TDModalAlert' => ['TDModalAlert/Assets/*.png']
---
+++ TDModalAlert/Classes/TDModalAlertView.swift
@@ -0,0 +1,92 @@
+//
+//  AOModalAlertView.swift
+//  TDModalAlert
+//
+//  Created by Duong Dinh on 12/2/19.
+//  Copyright © 2019 Duong Dinh. All rights reserved.
+//
+import UIKit
+
+public class TDModalAlertView: UIView {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var statusImage: UIImageView!
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var subheadLabel: UILabel!
+    
+    let nibName = "TDModalAlertView"
+    var contentView: UIView!
+    var timer: Timer?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpView()
+    }
+}
+
+// MARK: - Setup
+extension TDModalAlertView {
+    
+    private func setUpView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as? UIView
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        headlineLabel.text = ""
+        subheadLabel.text = ""
+        contentView.alpha = 0.0
+    }
+    
+    // Provide functions to update view
+    public func set(image: UIImage) {
+        self.statusImage.image = image
+    }
+    
+     public func set(headline text: String) {
+        self.headlineLabel.text = text
+    }
+    
+    public func set(subheading text: String) {
+        self.subheadLabel.text = text
+    }
+    
+    public override func didMoveToSuperview() {
+        // Fade in when added to superview
+        // Then add a timer to remove the view
+      self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        UIView.animate(withDuration: 0.15, animations: {
+            self.contentView.alpha = 1.0
+            self.contentView.transform = CGAffineTransform.identity
+        }) { _ in
+            self.timer = Timer.scheduledTimer(
+                timeInterval: TimeInterval(3.0),
+                target: self,
+                selector: #selector(self.removeSelf),
+                userInfo: nil,
+                repeats: false)
+        }
+    }
+    
+    @objc private func removeSelf() {
+        // Animate removal of view
+        UIView.animate(
+            withDuration: 0.15,
+            animations: {
+            self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            self.contentView.alpha = 0.0
+        }) { _ in
+            self.removeFromSuperview()
+        }
+    }
+}
