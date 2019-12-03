eda0b4d851537dbf54514115a258118d9bcd358a
Duong Dinh <duongdinh@duongs-mbp.clance.local>
2019-12-03 14:37:32 +0100

    Initial commit with cocoa pod template

---
+++ Example/Pods/Target Support Files/Pods-TDModalAlert_Tests/Pods-TDModalAlert_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDModalAlert_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TDModalAlert_Tests
+@end
---
+++ Example/Pods/Target Support Files/TDModalAlert/TDModalAlert-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TDModalAlert : NSObject
+@end
+@implementation PodsDummy_TDModalAlert
+@end
